113e1b51fceed817190ff702f425bdbe7cb02406
rohitkuk <rohit.kukreja01@gmail.com>
2021-07-19 19:59:20 +0530

    Initial Commit from Local

---
+++ main.py
@@ -0,0 +1,20 @@
+
+"""
+TODO:
+- OBject Detection on the Video
+    - Load YoloV3/YoloV5
+    - Detect Persons on a still Image
+    - Detect on Video
+    - Draw Bounding Boxes
+
+- Tracking
+    - Find a way to track the Bounding boxes and keeping count
+
+- Package Everything, Write a Document
+- Write some helper Functions.
+- Create a downloader in python
+
+"""
+
+
+
